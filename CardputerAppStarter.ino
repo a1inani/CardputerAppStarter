d7e7ade5d38f0437d8232ebd2b1bf52870d16bdd
Alinani Simukanga <a1inani@users.noreply.github.com>
2025-11-18 15:43:31 +0200

    Update CardputerAppStarter.ino

--- CardputerAppStarter.ino
+++ CardputerAppStarter.ino
@@ -6,16 +6,20 @@
 #include <include/ui.h>
 
 void setup() {
-  auto cfg = M5.config();
-  M5Cardputer.begin(cfg);
+    auto cfg = M5.config();
+    cfg.output_power = true;   // optional, powers internal components
 
-  initDisplay();
-  initKeyboard();
-  initUI();
+    M5Cardputer.begin(cfg);
+
+    initDisplay();
+    initKeyboard();
+    initUI();
 }
 
 void loop() {
-  lv_timer_handler();
-  handleKeyboard();
-  delay(5);
+    M5Cardputer.update();     // required for keyboard, button, RTC
+
+    lv_timer_handler();       // LVGL refresh
+    handleKeyboard();         // our key → LVGL routing
+    delay(5);
 }
