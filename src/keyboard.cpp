df758fae4da1874fc25f17aca61b6081807e6e58
Alinani Simukanga <a1inani@users.noreply.github.com>
2025-11-18 14:56:06 +0200

    Update keyboard.cpp

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -3,20 +3,26 @@
 #include <lvgl.h>
 
 void initKeyboard() {
-    // No initialization required.
+    // No initialization required
 }
 
 void handleKeyboard() {
-    // Only react when a new key state is detected
+    // Only act when there is a key state change
     if (M5Cardputer.Keyboard.isChange()) {
 
-        // Key pressed?
         if (M5Cardputer.Keyboard.isPressed()) {
-            uint8_t key = M5Cardputer.Keyboard.key;
 
-            // Send LVGL key event to the focused object
-            lv_event_send(lv_obj_get_focused(NULL), LV_EVENT_KEY, &key);
+            uint8_t key = M5Cardputer.Keyboard.keyCode();
+
+            // Get default group
+            lv_group_t *g = lv_group_get_default();
+            if (!g) return;
+
+            lv_obj_t *focused = lv_group_get_focused(g);
+            if (!focused) return;
+
+            // Send keyboard event to LVGL
+            lv_event_send(focused, LV_EVENT_KEY, &key);
         }
     }
 }
-
